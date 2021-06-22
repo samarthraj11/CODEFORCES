e306987ab4dca44efde9d36876889a2f65df2f86
Samarth Raj <72206930+samarthraj11@users.noreply.github.com>
2021-06-22 13:09:55 +0530

    11111ljsldjflsjaf

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define ll   long long
+#define pb   push_back
+#define ld   long double
+#define sz   size()
+#define lala for(ll i=0;i<n;i++)
+#define ed   "\n"
+#define ct   cout
+
+using namespace std;
+
+
+int main()
+{
+
+    ll c,d,k,n,f,x,y,z,cnt=0,res=0,mx=1;
+    ll sum=0;
+    bool ash=false;
+    string s;
+    //cin>>n;
+    //ll a[n];
+
+    ct<<1224.54%10;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+
